edd4a0aa6814dabc8fe56d6b30f71d818d216a8b
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-07-14 19:08:16 +0530

    Initial build, get basic user information

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
-#[test]
-fn it_works() {
+#![crate_name = "stackoverflow"]
+#![crate_type = "rlib"]
+extern crate curl;
+extern crate rustc_serialize;
+
+use client::Client;
+use user::UserService;
+
+pub struct Stackoverflow {
+    pub user: UserService,
 }
+
+impl Stackoverflow {
+    pub fn new() -> Stackoverflow {
+        let client = Client;
+        Stackoverflow {
+            user: UserService::new(client),
+        }
+    }
+}
+
+pub mod client;
+pub mod user;
---
+++ src/user.rs
@@ -0,0 +1,33 @@
+#[allow(unused_attributes)]
+
+use rustc_serialize::json;
+use client::Client;
+
+pub struct UserService {
+    client: Client,
+}
+
+impl UserService {
+
+    pub fn new(c: Client) -> UserService {
+        UserService { client: c }
+    }
+
+    pub fn get(self, name: &str) -> User {
+        let mut url = "http://api.stackexchange.com/2.2/users/2357380?site=stackoverflow".to_string();
+        url.push_str(name);
+
+        let req = self.client.request(url.as_ref());
+
+        let user: User = json::decode(req.as_ref()).unwrap();
+
+        return user;
+    }
+}
+
+#[allow(dead_code)]
+#[derive(RustcDecodable)]
+pub struct User {
+    pub items: String,
+    pub has_more: String,
+}
